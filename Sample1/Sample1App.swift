e3cc63e5ab39f65e93c6d2ef313b4f68e046f507
vickeyrajesh <vickey.rajesh@gmail.com>
2025-08-04 11:14:03 +0530

    Initial Commit

---
+++ Sample1/Sample1App.swift
@@ -0,0 +1,20 @@
+//
+//  Sample1App.swift
+//  Sample1
+//
+//  Created by Rajesh Yadav on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Sample1App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
